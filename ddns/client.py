84320edb315598a4981b1fba1c0b57a496004887
Yehuda Deutsch <yeh@uda.co.il>
2017-04-23 02:53:46 +0300

    Better zone and data handling

--- ddns/client.py
+++ ddns/client.py
@@ -1,10 +1,14 @@
+import random
+
+import six
 from twisted.names import dns, error
 from twisted.internet import defer
 from twisted.names.common import ResolverBase
 
 
 class DynamicResolver(ResolverBase):
-    REDIS_KEY_FORMAT = 'DDNS:{}:{}'
+    REDIS_KEY_FORMAT = 'DDNS:ZONE:{}'
+    REDIS_FIELD_FORMAT = '{}:{}'
 
     def __init__(self, redis):
         """
@@ -19,6 +23,10 @@ class DynamicResolver(ResolverBase):
 
     def lookupAddress(self, name, timeout=None):
         def return_record(record):
+            if isinstance(record, six.string_types):
+                record = record.strip()
+                if '\x1e' in record:
+                    record = random.choice(record.split('\x1e'))
             if record:
                 return defer.succeed([
                     (dns.RRHeader(
@@ -38,5 +46,7 @@ class DynamicResolver(ResolverBase):
         return d
 
     def _get_a_record(self, name):
-        rr_key = self.REDIS_KEY_FORMAT.format('A', name)
-        return self.redis.get(rr_key)
+        host, domain = name.split('.', 1)
+        rr_key = self.REDIS_KEY_FORMAT.format(domain)
+        rr_field = self.REDIS_FIELD_FORMAT.format(host, 'A')
+        return self.redis.hget(rr_key, rr_field)
