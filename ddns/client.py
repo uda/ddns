857c620b7114649a2a8b44049125bf449bfd2de5
Yehuda Deutsch <yeh@uda.co.il>
2017-04-22 13:37:31 +0300

    Started the redis resolver

--- ddns/client.py
+++ ddns/client.py
@@ -1,4 +1,18 @@
+from twisted.names import dns, client
 
 
-class DynamicResolver(object):
-    pass
+class DynamicResolver(client.Resolver):
+    REDIS_KEY_FORMAT = 'DNS:{record}:'
+
+    def __init__(self, redis):
+        """
+        :param txredis.client.RedisClient redis: 
+        """
+        self.redis = redis
+        client.Resolver.__init__(self)
+
+    def _get_rr(self, type):
+        record = self.redis.get('')
+
+    def lookupAddress(self, name, timeout=None):
+        return ''
