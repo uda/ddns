49d61be876ed1685656ec6c0df2f2b78879b444b
Yehuda Deutsch <yeh@uda.co.il>
2017-05-02 01:24:56 +0300

    Adds some actions

--- app.py
+++ app.py
@@ -3,14 +3,16 @@ from klein import Klein
 
 app = Klein()
 
-app.route('/')(main.main_index)
+main.main_index = app.route('/')(main.main_index)
 
 with app.subroute('/user') as app:
-    app.route('/')(user.user_index)
-    app.route('/view/<string:user>')(user.user_view)
+    user.user_index = app.route('/')(user.user_index)
+    user.user_login_form = app.route('/login', methods=['GET'])(user.user_login_form)
+    user.user_login = app.route('/login', methods=['POST'])(user.user_login)
+    user.user_view = app.route('/view/<string:user>')(user.user_view)
 
 with app.subroute('/domain') as app:
-    app.route('/')(domain.domain_index)
-    app.route('/view/<string:name>')(domain.domain_view)
+    domain.domain_index = app.route('/')(domain.domain_index)
+    domain.domain_view = app.route('/view/<string:name>')(domain.domain_view)
 
 app.run("localhost", 8080)
--- web/user.py
+++ web/user.py
@@ -1,5 +1,17 @@
+from twisted.internet import defer
+
+
 def user_index(request):
     return 'Hello, user!'
+
+
+def user_login_form(request):
+    return 'User login <form action="/user/login" method="post"><button type="submit">Login</button></form>'
+
+
+def user_login(request):
+    request.redirect('/')
+    return defer.succeed(None)
 
 
 def user_view(request, user):
